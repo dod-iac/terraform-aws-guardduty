0dc4b4a7bd505fe5049f467fcdc584308d7fc00c
Chris Gilmer <chris.gilmer@gmail.com>
2021-03-12 15:10:34 -0800

    Update repo to match template repo (#2)
    * Update repo to match template repo
    
    * Fix

--- versions.tf
+++ versions.tf
@@ -1,7 +1,11 @@
+
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.13"
 
   required_providers {
-    aws = ">= 3.4.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
   }
 }
